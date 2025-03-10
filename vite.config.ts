b09fe1d615e673ef0d5b6b9219a5f7312141880b
eshta <eshtadevatha06@gmail.com>
2025-03-10 20:40:20 +0530

    well

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
     allowedHosts: ['portfolio-eshta-devatha-k.onrender.com']
   }
 });
