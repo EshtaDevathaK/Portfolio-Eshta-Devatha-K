e46968ec9e3946da66cdfe94016347b25f920231
eshta <eshtadevatha06@gmail.com>
2025-03-10 20:36:48 +0530

    SUccess

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: Number(process.env.PORT) || 3000,
-    host: '0.0.0.0'
-  },
+    host: '0.0.0.0',
+    allowedHosts: ['portfolio-eshta-devatha-k.onrender.com']
+  }
 });
